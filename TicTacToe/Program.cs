cee2d1c9a82d131df55507398ab7616d14047cfe
Tanner Funk <tannerfunk4@gmail.com>
2023-01-25 09:53:27 -0700

    comments

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -6,17 +6,20 @@ namespace TicTacToe
     {
         static void Main(string[] args)
         {
+            // Greet User (they're a homie obviously)
             Console.WriteLine("Sup User!");
 
+            // Declare and initialize our board.
             PrintBoard printBoard = new PrintBoard();
-
             string[] boardPosition = new string[9];
 
+            //Fill the board with the keys
             for (int i = 0; i < 9; i++)
             {
                 boardPosition[i] = Convert.ToString(i);
             }
 
+            
             int playerMove = 0;
             bool player1Turn = true;
 
@@ -26,8 +29,12 @@ namespace TicTacToe
 
             Console.WriteLine("0 is the top left position, 8 is the bottom right position.");
 
+            // Loop through game
+
+            // Gets the move and sends puts it on the board and sends the board to the support class
             while (noWinner == "N")
             {
+                //player 1's turn toggles to player 2 and vice versa
                 if (player1Turn)
                 {
                     Console.WriteLine("Enter the position for player 1: ");
@@ -39,7 +46,6 @@ namespace TicTacToe
                     }
                     boardPosition[playerMove] = "X";
                     player1Turn = false;
-
                 }
                 else
                 {
@@ -54,13 +60,14 @@ namespace TicTacToe
                     player1Turn = true;
                     
                 }
-
+                // calls support class
                 printBoard.Print(boardPosition);
                 noWinner = PrintBoard.Victory(boardPosition);
 
 
             }
 
+            //checks for winner or draw and prints results
             if (noWinner == "D")
             {
                 Console.WriteLine("It's a draw!");
--- TicTacToe/SupportClass/Support.cs
+++ TicTacToe/SupportClass/Support.cs
@@ -73,7 +73,7 @@ class PrintBoard
         }
         else
         {
-            return "N"; // Nothin happens;
+            return "N"; // Nothin happens
         }
 
     }
